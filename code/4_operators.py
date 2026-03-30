462b3a5b0b0049a5dfca2817cbc5da8968eaae99
iot-이강혁 <rhdrur44018@a.ut.ac.kr>
2026-03-30 11:28:07 +0900

    Add files via upload

---
+++ code/4_operators.py
@@ -0,0 +1,46 @@
+# 4_operators.py
+a = 132
+b = 45
+
+fmt0 = '{:<10}' # 변수 + 공백 최대 10개
+fmt1 = '0b{:08b} 0x{:02x} {:3}' # 0ㅠ________ 2진법 8개, 0x_______ 16진법 2개, 10진법 3개
+
+# bit &
+print(fmt0.format('a'), fmt1.format(a,a,a))
+print(fmt0.format('b'), fmt1.format(b,b,b))
+
+n = 30
+print('-' * n)
+
+print(fmt0.format('a & b'), fmt1.format(a&b,a&b,a&b))
+
+# bit OR |
+print(fmt0.format('a'), fmt1.format(a,a,a))
+print(fmt0.format('b'), fmt1.format(b,b,b))
+print('-' * n)
+print(fmt0.format('a | b'), fmt1.format(a|b,a|b,a|b))
+
+# bit XOR ^
+print("\nbit XOR ^")
+print(fmt0.format('a'), fmt1.format(a,a,a))
+print(fmt0.format('b'), fmt1.format(b,b,b))
+print('-' * n)
+print(fmt0.format('a ^ b'), fmt1.format(a^b,a^b,a^b))
+
+# bit NOT ~
+print("\nbit NOT ~")
+print(fmt0.format('a'), fmt1.format(a,a,a))
+print('-' * n)
+print(fmt0.format('~a'), fmt1.format(~a&0xFF,~a&0xFF,~a&0xFF))
+
+# bit 왼쪽 쉬프트 연산
+print("\nbit 왼쪽 쉬프트 <<")
+print(fmt0.format('a'), fmt1.format(a,a,a))
+print('-' * n)
+print(fmt0.format('a << 2'), fmt1.format(a<<2&0xFF,a<<2&0xFF,a<<2&0xFF))
+
+# bit 오른쪽 쉬프트 연산
+print("\nbit 오른쪽 쉬프트 >>")
+print(fmt0.format('a'), fmt1.format(a,a,a))
+print('-' * n)
+print(fmt0.format('a >> 2'), fmt1.format(a>>2&0xFF,a>>2&0xFF,a>>2&0xFF))
